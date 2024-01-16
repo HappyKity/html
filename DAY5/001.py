ba308a05a5b93880b3d5beea95fe737aac76688a
hwang <hssxman0@naver.com>
2024-01-16 13:24:10 +0900

    html

---
+++ DAY5/001.py
@@ -0,0 +1,18 @@
+from random import random
+import datetime as d
+import random
+
+
+def aaa():
+    return "hello"
+
+
+def bbb():
+    return "까꿍"
+
+
+random.random()
+
+
+print(d.datetime.now())
+print(d.datetime.now().date().weekday())
